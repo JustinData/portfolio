20d27716459260f81ba78ffb9fe452e9c9eda9ce
JustinGA <justinkemmerer@gmail.com>
2014-01-13 19:38:04 -0500

    stuff

--- app/assets/javascripts/portfolio.js
+++ app/assets/javascripts/portfolio.js
@@ -1,23 +1,46 @@
 $( document ).ready(function() {
-	$(window).bind('scroll',function() {
+	$(window).bind('scroll',function(e) {
 		var vertPosition = $(window).scrollTop(); 
-		var totalHeight = $('#sticky').offset().top; 
-		var finalSize = totalHeight - vertPosition; 
+		var stickyHeight = $('#sticky').offset().top; 
+		var stickyTop = stickyHeight - vertPosition; 
 
-		if( finalSize <= 0 ) {
+        var $cc = $('#content-container');
+        var ccHeight = $cc.offset().top;
+        var ccTop = ccHeight - vertPosition;
+
+
+
+		if( stickyTop <= 0 ) {
+
             $('#sticky').css({
                 'position': 'fixed',
                 'top': 0
             });
-            $('#content-container').css({
-                'position': 'fixed',
-                'top': 21
-            })
-            $('#screenshot').remove();
+
+            $("html, body").animate({scrollTop: ccHeight}, 1250);
+
 		} else {
             $('#sticky').css({
                 'position': 'absolute'
             })
 		}
+
+        if( ccTop <= 30 ) {
+            $cc.css({
+                'position': 'fixed',
+                'top': 30
+            });
+            $('#screenshot').remove();
+        }
+
+        $(window).stellar();
+
+        // var s1 = 50;
+        // var s2 = 100;
+        // var s3 = 200;
+
+        // var pageY = e.pageY - ($(window).height() / 2);
+
+        // $('#photo').css("background-position", ("1px " + s2 / $(window).height() * pageY -1) + "px");
 	});
 });
