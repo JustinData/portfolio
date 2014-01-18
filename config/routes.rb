9db7336f4eb7fc0f9275dade5d6440847fe5eb18
JustinGA <justinkemmerer@gmail.com>
2014-01-18 17:59:52 -0500

    main design complete,

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Portfolio::Application.routes.draw do
  
 	root 'welcome#index'
 
-	resource :welcome, only: [:index]
+	# resource :welcome, only: [:index]
+
+	resources :projects, only: [:index]
+
+	resources :experiences, only: [:index]
+
+	resources :contacts, only: [:index]
 
 	namespace :json, constraints: { format: 'json' } do
 
